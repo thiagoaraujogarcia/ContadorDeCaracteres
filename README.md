3bdba3383a6c302d5f4c3eb578f69f404f77db17
thiagoaraujogarcia <32115307+thiagoaraujogarcia@users.noreply.github.com>
2018-07-23 22:57:59 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ContadorDeCaracteres
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import ReactDOM, { findDOMNode } from "react-dom";
+
+import "./styles.css";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      digitado: 0
+    };
+  }
+
+  // amountCaracter(){
+  //   const conteudo = campo.value();
+  //   const amount = conteudo.length;
+  // }
+
+  handleType() {
+    const amount = findDOMNode.length;
+    console.log(amount);
+    if (amount > 0) {
+      this.state.digitado === amount;
+      //this.setState({ digitado });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Contador de Caracteres Digitados</h1>
+        <h2>Entre com os dados</h2>
+        <textarea onInputCapture={this.handleType()} rows="8" cols="40" />
+        <p>Quantidade de Caracteres: {this.state.digitado}</p>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
